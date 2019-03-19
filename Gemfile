e1ae3c83a09f9ecde586513908e41784bda27d14
MikeMaven <mikemaven@gmail.com>
2019-03-19 15:21:08 -0400

    Updated master

--- Gemfile
+++ Gemfile
@@ -3,11 +3,12 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'rake'
+gem 'csv'
 
 group :test, :development do
   gem 'pry'
   gem 'capybara'
   gem 'rspec'
   gem 'launchy'
+  gem 'csv'
 end
-
--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'csv'
 
 configure :development, :test do
   require 'pry'
@@ -9,8 +10,22 @@ Dir[File.join(File.dirname(__FILE__), 'lib', '**', '*.rb')].each do |file|
   require file
   also_reload file
 end
+require_relative 'app/models/article.rb'
 
-get '/' do
-  @title = "Hello World"
-  erb :index
+def create_articles_array
+  articles = []
+  CSV.foreach('news.csv', headers: true) do |row|
+    articles << Article.new(
+      row['id'],
+      row['title'],
+      row['description'],
+      row['url']
+    )
+  end
+  articles
+end
+
+get '/articles' do
+  @articles = create_articles_array
+  erb :articles
 end
